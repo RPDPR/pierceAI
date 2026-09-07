68ff3aa9d7e157137c697284d3fe4550526ee544
Igor Krotinov <dancigor@gmail.com>
2026-09-07 20:31:45 +0400

    its 90/10 now

--- src/services/generator.py
+++ src/services/generator.py
@@ -20,7 +20,7 @@ class PierceGeneratorService:
         bottom_raw: str
     ) -> BytesIO:
 
-        if pool_images and random.random() < 0.8:
+        if pool_images and random.random() < 0.9:
             bg_path = os.path.join(guild_pool_dir, random.choice(pool_images))
             base_img = Image.open(bg_path).convert("RGB")
         else:
